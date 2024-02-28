b48eee9db3045d451ac312ce25ece69939be8c08
The-Snollygoster <157751521+The-Snollygoster@users.noreply.github.com>
2024-02-28 20:22:15 +0000

    Fix multiplication bug
    The if statement symbol and the operator symbol
    were different for the multiplication function
    resulting in no computations and a cascade into
    subsequent uses. Quick fix of changing the if 'x'
    to if '*'.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function operate(a, b, c) {
         addition(a,c);
     } else if (b == '-') {
         subtraction(a,c);
-    } else if (b == 'x') {
+    } else if (b == '*') {
         multiplication(a,c);
     } else if (b == '/') {
         division(a,c);
