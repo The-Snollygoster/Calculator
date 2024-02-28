43ec0ae40408d56b47bf59b40d504890e6c51d3b
The-Snollygoster <157751521+The-Snollygoster@users.noreply.github.com>
2024-02-28 18:40:33 +0000

    Added basic functions and clear
    I've added the basic functions for calculations.
    An operate function that will take the arguments
    and call the relevant function and a clear button
    linked to the display to wipe it clean.

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-// Calculator functions!
+let argumentOne = '';
+let operator = '';
+let argumentTwo = '';
 
-// addition
-// subtraction
-// multiplication
-// division
+function addition(x, y) {
+    return x + y;
+};
+function subtraction(x, y) {
+    return x - y;
+};
+function multiplication(x, y) {
+    return x * y;
+};
+function division(x, y) {
+    return x / y;
+};
+
+function operate(a, b, c) {
+    if (b == '+') {
+        addition(a,c);
+    } else if (b == '-') {
+        subtraction(a,c);
+    } else if (b == 'x') {
+        multiplication(a,c);
+    } else if (b == '/') {
+        division(a,c);
+    }
+};
+
+// Clear button functionality
+const clearButton = document.querySelector('#clearElse');
+let display = document.querySelector('.display');
+clearButton.addEventListener('click', () => display.textContent = '');
